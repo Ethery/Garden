032b4d43ac15011bf86051113f02050028db1f84
Ethery <loic.meunier74@gmail.com>
2023-07-22 12:15:35 +0200

    Added rules and world config files.

---
+++ Assets/Scripts/World/Config/RulesConfig.cs
@@ -0,0 +1,7 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewRulesConfig", menuName = "Game/World/Config/Rules")]
+public class RulesConfig : ScriptableObject
+{
+}
---
+++ Assets/Scripts/World/Config/WorldConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewWorldConfig",menuName="Game/World/Config")]
+public class WorldConfig : ScriptableObject
+{
+	public RulesConfig Rules => m_rules;
+
+
+	[SerializeField]
+	private RulesConfig m_rules;
+}
