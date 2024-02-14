a08d019adace3c8fc4f65d5f3b8461538a6173e1
Ethery <loic.meunier74@gmail.com>
2024-02-14 22:24:08 +0100

    Fixed cursor

--- Assets/Scripts/System/Inputs/GridCursor.cs
+++ Assets/Scripts/System/Inputs/GridCursor.cs
@@ -21,6 +21,16 @@ namespace Game.Systems.Inputs
 			RegisterInputs(false);
 		}
 
+		private void Update()
+		{
+			if(m_oldMousePosition != m_currentMousePosition)
+			{
+				GameManager.Instance.World.TileMap.SetTile(m_oldMousePosition, null);
+				GameManager.Instance.World.TileMap.SetTile(m_currentMousePosition, m_cursorTile);
+				m_oldMousePosition = m_currentMousePosition;
+			}
+		}
+
 		private void RegisterInputs(bool register)
 		{
 			InputManager.RegisterInput(InputManager.Input.MOUSE_POS, new InputManager.InputEvent(OnMousePosPerf, UnityEngine.InputSystem.InputActionChange.ActionPerformed), register);
@@ -34,15 +44,18 @@ namespace Game.Systems.Inputs
 			if(m_plane.Raycast(ray, out distanceToPlane))
 			{
 				Vector3 hitPoint = ray.GetPoint(distanceToPlane);
-				Vector3Int hitOnTilemap = hitPoint.ToVector3Int();
-				GameManager.Instance.World.TileMap.SetTile(hitOnTilemap, m_cursorTile);
-
-				Debug.Log($"hit ({hitPoint}/{hitOnTilemap})");
+				//hitPoint = Grid.Swizzle(GameManager.Instance.World.Grid.cellSwizzle, hitPoint);
+				m_currentMousePosition = GameManager.Instance.World.TileMap.WorldToCell(hitPoint);
 			}
 		}
 
 		[NonSerialized]
+		private Vector3Int m_currentMousePosition;
+		[NonSerialized]
+		private Vector3Int m_oldMousePosition;
+		[NonSerialized]
 		private Plane m_plane;
+
 		[SerializeField]
 		private Tile m_cursorTile;
 	}
--- Assets/Scripts/Tools/Vector3Extensions.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class Vector3Extensions
-{
-	public static Vector3Int ToVector3Int(this Vector3 source)
-	{
-		return new Vector3Int(Mathf.RoundToInt(source.x), Mathf.RoundToInt(source.y), Mathf.RoundToInt(source.z));
-	}
-}
